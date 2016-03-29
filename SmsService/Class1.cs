ab53c8168699094182d1d8b3276bbedd9403006a
Hessam Hoseini <Hessam Hoseini>
2016-03-29 19:16:09 +0330

    Create SmsService project

---
+++ SmsService/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmsService
+{
+    public class Provider
+    {
+        public string Name { get; set; }
+
+        public string CompanyName { get; set; }
+
+
+
+    }
+}
